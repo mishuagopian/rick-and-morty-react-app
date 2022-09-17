8f5894978cb6917d65f279e45c8751030e308cc4
mishuagopian <mishuagopian@gmail.com>
2022-09-16 21:02:37 -0300

    ran format script

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 export const store = configureStore({
-  reducer: {
-  },
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;

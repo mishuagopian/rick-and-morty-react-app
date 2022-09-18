130b7aa8b30f586ba8d62f0d908daa76134fb7e7
mishuagopian <mishuagopian@gmail.com>
2022-09-17 22:20:19 -0300

    fixed characters list layout in md devices to fill the whole width of the screen

--- src/screens/CharactersListScreen/styles.ts
+++ src/screens/CharactersListScreen/styles.ts
@@ -7,12 +7,15 @@ export const styles = {
     justifyContent: "center",
     paddingLeft: responsive ? drawerWidth : 0,
   }),
-  list: {
+  list: (theme: any) => ({
     display: "flex",
     justifyContent: "space-evenly",
     flexWrap: "wrap",
     marginBottom: "50px",
-  },
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+    },
+  }),
   pagination: {
     alignSelf: "center",
     backdropFilter: "blur(10px)",
